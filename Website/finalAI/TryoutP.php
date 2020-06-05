d40bac910256839b2be4580369024b1d71597b4c
ysyloveu <41413469+ysyloveu@users.noreply.github.com>
2020-06-05 16:27:33 +0800

    Update TryoutP.php

--- Website/finalAI/TryoutP.php
+++ Website/finalAI/TryoutP.php
@@ -83,54 +83,5 @@ for ($x = 0; $x <= $keycount-1;$x++){
     print $a." , ";
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// echo($result[0]);
-// print_r(array_filter($result))
-
-
-// // The data to send to the API
-// $postData = array(
-//     'url' => 'https://www.straitstimes.com/singapore/move-to-phase-2-of-covid-19-reopening-to-be-decided-mid-june-dining-in-use-of-sports',
-//     'title' => 'Move to Phase 2 of Covid-19 reopening to be decided mid-June; dining-in, use ring this phase, Singapore News & Top Stories - The Straits Times',
-//     'content' => 'We have been experiencing some problems with subscriber log-ins and apologise for the inconvenience caused. 
-//     Until we resolve the issues, subscribers need not log in to access ST Digital articles. But a log-in is still required for our PDFs.'
-// );
-
-// // Setup cURL
-// $ch = curl_init('http://localhost:8080/fakebox/check');
-// curl_setopt_array($ch, array(
-//     CURLOPT_POST => TRUE,
-//     CURLOPT_RETURNTRANSFER => TRUE,
-//     CURLOPT_HTTPHEADER => array(
-//         // 'Authorization: '.$authToken,
-//         'Content-Type: application/json; charset=utf-8'
-//     ),
-//     CURLOPT_POSTFIELDS => json_encode($postData)
-// ));
-
-// // Send the request
-// $response = curl_exec($ch);
-
-// // Check for errors
-// if($response === FALSE){
-//     die(curl_error($ch));
-// }
-
-// // Decode the response
-// $responseData = json_decode($response, TRUE);
-
-// // Print the date from the response
-// echo $responseData['published'];
-
 ?>
 
